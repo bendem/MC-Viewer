92e102792b641d570d1bd19d5c46cf610dbe8d3b
bendem <bendembd@gmail.com>
2014-01-05 16:56:08 +0100

    Fully implemented now
    + Changed indentation to spaces

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-## TO DO
+# MC-Viewer
 
-- Write the app
-- Write the readme
+Chrome extension to easily know who's online on our minecraft server (+notifications)...
+
+## Known bugs
+
+Player diff is made by the server, it's sick, I'll change tomorrow...
--- js/updateViewer.js
+++ js/updateViewer.js
@@ -1,72 +1,79 @@
-var timer = 5; // Update period in minutes
+var timer = 1; // Update period in minutes
 
 chrome.alarms.create("updateTrigerer", {
-	periodInMinutes: timer,
+    periodInMinutes: timer,
 });
 
 chrome.alarms.onAlarm.addListener(function(alarm) {
-	update();
+    update();
 });
 
 function update() {
-	// Update the viewer and send notifications if join or quit
-	var xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = xhrReadyHandler; // Implemented elsewhere.
-	xhr.open("GET", "http://mc-viewer.bendem.be/ajax.php", true);
-	xhr.send();
+    // Update the viewer and send notifications if join or quit
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = xhrReadyHandler; // Implemented elsewhere.
+    xhr.open("GET", "http://mc-viewer.bendem.be/ajax.php", true);
+    xhr.send();
 }
 
 function xhrReadyHandler() {
-	if(this.readyState == XMLHttpRequest.DONE) {
-		console.log(JSON.parse(this.response));
-		var response = JSON.parse(this.response)
-			, diff = response.diff                 // Diff should not be handled server-side !
-			, players = response.players;
+    if(this.readyState != XMLHttpRequest.DONE) {
+        return;
+    }
+    // console.log(JSON.parse(this.response));
+    var response = JSON.parse(this.response)
+        , diff = response.diff                 // Diff should not be handled server-side !
+        , players = response.players;
 
-		var notifTemplate = {
-			type: "basic",
-			iconUrl: "http://mc-viewer.bendem.be/skin.php?t=head&s=64&u=",
-			title: " has ",
-			message: " has just "
-		}, msgEnd = " from mc.bendem.be", notifDetails;
+    var notifTemplate = {
+        type: "basic",
+        iconUrl: "http://mc-viewer.bendem.be/skin.php?t=head&s=64&u=",
+        title: " has ",
+        message: " has just "
+    }, msgEnd = " from mc.bendem.be", notifDetails;
 
-		// Send connection notification
-		for (var i = diff.connections.length - 1; i >= 0; i--) {
-			notifDetails = notifTemplate;
-			notifDetails.iconUrl += diff.connections[i];
-			notifDetails.title = diff.connections[i] + notifDetails.title + "connected";
-			notifDetails.message = diff.connections[i] + notifDetails.message + "connected" + msgEnd;
+    // Send connection notification
+    for (var i = diff.connections.length - 1; i >= 0; i--) {
+        notifDetails = notifTemplate;
+        notifDetails.iconUrl += diff.connections[i];
+        notifDetails.title = diff.connections[i] + notifDetails.title + "connected";
+        notifDetails.message = diff.connections[i] + notifDetails.message + "connected" + msgEnd;
 
-			chrome.notifications.create(
-				diff.connections[i] + "_connected",
-				notifDetails,
-				function() {}
-			);
-		}
-		// Send disconnection notification
-		for (var i = diff.disconnections.length - 1; i >= 0; i--) {
-			notifDetails = notifTemplate;
-			notifDetails.iconUrl += diff.disconnections[i];
-			notifDetails.title = diff.disconnections[i] + notifDetails.title + "disconnected";
-			notifDetails.message = diff.disconnections[i] + notifDetails.message + "disconnected" + msgEnd;
+        chrome.notifications.create(
+            diff.connections[i] + "_connected",
+            notifDetails,
+            function() {}
+        );
+    }
+    // Send disconnection notification
+    for (var i = diff.disconnections.length - 1; i >= 0; i--) {
+        notifDetails = notifTemplate;
+        notifDetails.iconUrl += diff.disconnections[i];
+        notifDetails.title = diff.disconnections[i] + notifDetails.title + "disconnected";
+        notifDetails.message = diff.disconnections[i] + notifDetails.message + "disconnected" + msgEnd;
 
-			chrome.notifications.create(
-				diff.disconnections[i] + "_disconnected",
-				notifDetails,
-				function() {}
-			);
-		}
+        chrome.notifications.create(
+            diff.disconnections[i] + "_disconnected",
+            notifDetails,
+            function() {}
+        );
+    }
 
-		// Update avatars
-		// for (var i = players.length - 1; i >= 0; i--) {
-		// 	players[i]
-		// }
+    // Update avatars in the popup
+    var view = document.querySelector('#main');
+    if(players.length == 0) {
+        view.innerHTML = "Pas de joueurs connectés...";
+    } else {
+        view.innerHTML = "";
+    }
+    for (var i = players.length - 1; i >= 0; i--) {
+        view.innerHTML += '<img src="' + notifTemplate.iconUrl + players[i] + '" alt="' + players[i] + '"/>';
+    }
 
-		// Display the number of connected people on the server right now
-		chrome.browserAction.setBadgeText({
-			text: players.length;
-		});
-	}
+    // Update badge number
+    chrome.browserAction.setBadgeText({
+        text: players.length.toString()
+    });
 }
 
 update();
